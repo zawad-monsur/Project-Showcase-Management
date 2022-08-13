d2bb57d00ac0e2eea7fd282096cb2105e3950e49
Debopom Sutradhar <dsutradhar201046@bscse.uiu.ac.bd>
2022-08-14 02:05:28 +0600

    connecting db and showing data from project table

---
+++ Manage_projects_faculty.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+     
+
+     session_start();
+
+     include_once 'dbconnect.php';
+     $sql = "SELECT * FROM projects WHERE faculty_id = 1";
+    ?>
+     <table align="center" style="width:90%; line-height:200%;"> 
+	<tr> 
+		<th colspan="6"><h2>Projects</h2></th> 
+		</tr> 
+			  <th>Project Title </th> 
+              <th>Description</th> 
+              <th>Category </th> 
+               
+
+                   
+		</tr> 
+        
+       
+		
+		<?php
+            $rs = $conn-> query($sql); 
+            
+         while($rows=mysqli_fetch_array($rs)) 
+		{ 
+		?>
+        <tr> 
+		<td><?php echo $rows['project_title']; ?></td> 
+        <td><?php echo $rows['description']; ?></td> 
+        <td><?php echo $rows['category']; ?></td> 
+        </tr>
+        
+        
+    <?php
+        }?>
+    </table>
+ 
+
+
+</body>
+</html>
